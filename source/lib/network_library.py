6e430a0d875070164b6ef3d6d1f394c872c53874
melvyn <peignonmel@eisti.eu>
2017-03-17 22:01:35 +0800

    Add a library to manage the graph and a library to collect the tweet of a user

--- source/lib/network_library.py
+++ source/lib/network_library.py
@@ -2,9 +2,10 @@ import networkx as nx
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
 from elasticsearch import Elasticsearch
-import matplotlib.pyplot as plt
+import pickle
+import os.path
 
-from source import DEFINITIONS_ROOT
+from source.__init__ import DEFINITIONS_ROOT
 
 
 class SocialGraph:
@@ -18,8 +19,8 @@ class SocialGraph:
 
     @classmethod
     def build_graph_from_seed(cls, name):
-        obj = cls()
-        obj.name = name
+        print("Building graph from seed")
+        obj = cls(name=name)
         query = "SELECT twitter_id FROM {0};"
         seeds = obj.session.execute(query.format(obj.seed_data_table))
         for seed in seeds:
@@ -35,39 +36,31 @@ class SocialGraph:
                     follower_ids.append(row.friend_follower_id)
             if friend_ids:
                 for friend_id in friend_ids:
-                    obj.graph.add_edge(seed.twitter_id, friend_id)
+                    obj.graph.add_edge(int(seed.twitter_id), int(friend_id))
             if follower_ids:
                 for follower_id in follower_ids:
-                    obj.graph.add_edge(follower_id, seed.twitter_id)
+                    obj.graph.add_edge(int(follower_id), int(seed.twitter_id))
         obj.modified_graph = obj.graph
         return obj
 
     @classmethod
     def build_seed_graph(cls, name):
-        obj = cls()
-        obj.name = name
+        obj = cls(name=name)
+        print("Build the graph from the seed present in the table {}".format(obj.seed_data_table))
         query = "SELECT twitter_id FROM {0};"
         seeds = obj.session.execute(query.format(obj.seed_data_table))
         for seed in seeds:
             obj.seeds.append(int(seed.twitter_id))
         obj.graph.add_nodes_from(obj.seeds)
         for seed in obj.seeds:
-            query = "SELECT friend_follower_id, is_friend, is_follower FROM {0} WHERE node_id={1}"
+            query = "SELECT friend_follower_id, is_friend, is_follower FROM {0} WHERE user_id={1}"
             statement = SimpleStatement(query.format(obj.network_table, seed), fetch_size=1000)
-            friend_ids = []
-            follower_ids = []
             for row in obj.session.execute(statement):
                 if row.friend_follower_id in obj.seeds:
                     if row.is_friend:
-                        friend_ids.append(row.friend_follower_id)
-                    if row.is_follower:
-                        follower_ids.append(row.friend_follower_id)
-            if friend_ids:
-                for friend_id in friend_ids:
-                    obj.graph.add_edge(seed, friend_id)
-            if follower_ids:
-                for follower_id in follower_ids:
-                    obj.graph.add_edge(follower_id, seed)
+                        obj.graph.add_edge(seed, row.friend_follower_id)
+                    elif row.is_follower:
+                        obj.graph.add_edge(row.friend_follower_id, seed)
         obj.modified_graph = obj.graph
         return obj
 
@@ -83,26 +76,50 @@ class SocialGraph:
         if g == 'original':
             graph_to_save = self.graph
         if path:
+            print("Save graph to location: {}".format(DEFINITIONS_ROOT+path))
             nx.write_gml(graph_to_save, DEFINITIONS_ROOT+path, stringizer=str)
         else:
-            nx.write_gml(graph_to_save, DEFINITIONS_ROOT+"/data/graph/{}.gml".format(self.name), stringizer=str)
+            print("Save graph to lacation: {0}/data/graph/{1})".format(DEFINITIONS_ROOT, self.name))
+            nx.write_gml(graph_to_save, DEFINITIONS_ROOT+"/data/graph/{0}/{1}.gml".format(self.name, self.name)
+                         , stringizer=str)
 
-    def __init__(self):
+    @staticmethod
+    def pickle_loader(file_path):
+        if not os.path.exists(file_path):
+            print("Failed to load file at location: {} ".format(file_path))
+            return False
+        else:
+            print("Load file: {}".format(file_path))
+            return pickle.load(open(file_path, "rb"))
+
+    def get_nodes_by_betweenness_centrality(self, nb_node, normalized=True):
+        print("Trying to load the betweenness centrality from file...")
+        file_path = DEFINITIONS_ROOT+"/data/graph/{0}/{1}_betweenness_centrality.p".format(self.name, str(nb_node))
+        betweenness_centrality = self.pickle_loader(file_path)
+        if not betweenness_centrality:
+            print("Failed to load an existent betweenness centrality")
+            print("Computing the betweenness centrality")
+            betweenness_centrality = nx.algorithms.betweenness_centrality(
+                self.graph, k=nb_node, normalized=normalized)
+            pickle.dump(betweenness_centrality, open(file_path, "wb"))
+        else:
+            print("Load betweenness centrality from file")
+        return betweenness_centrality
+
+    def __init__(self, name):
         self.graph = nx.DiGraph()
         self.modified_graph = self.graph
         self.seeds = []
-        self.name = ""
+        self.name = name
+        if not os.path.exists(DEFINITIONS_ROOT+"/data/graph/{0}".format(name)):
+            os.makedirs(DEFINITIONS_ROOT+"/data/graph/{0}".format(name))
 
 
+"""
+HELPERS FUNCTIONS
+"""
 
-print("Building the graph...")
-graph = SocialGraph.build_seed_graph("manual_seed")
-print(nx.dag_longest_path(graph.graph))
-# print("Removing leaves...")
-# graph.remove_useless_leaves()
-# print("Computing the betweenness centrality")
-#betweenness_centrality = nx.algorithms.betweenness_centrality(graph.modified_graph, k=400)
-# print(betweenness_centrality)
-# print("Saving the graph: {} to source/data/graph".format(graph.name))
-# graph.export_gml(g='original')
-# print("Graph exported!")
+
+def sort_node_by_importance(nodes_dictionnary):
+    sorted_nodes = sorted(nodes_dictionnary.items(), key=lambda x: x[1], reverse=True)
+    return sorted_nodes
---
+++ source/lib/tweeter_library.py
@@ -0,0 +1,156 @@
+from elasticsearch import Elasticsearch
+from cassandra.cluster import Cluster
+from elasticsearch import helpers
+import tweepy
+from cassandra.concurrent import execute_concurrent_with_args
+
+
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    from source.settings import settings
+
+TWEETS_MAPPING = {
+                    "template": "twitter*",
+                    "settings": {
+                        "number_of_shards": 1,
+                        "index.mapping.total_fields.limit": 2000,
+                    },
+                    "mappings": {
+                        "tweets": {
+                            "properties": {
+                                "coordinates": {
+                                    "properties": {
+                                        "coordinates": {
+                                            "type": "geo_point"
+                                        },
+                                        "type": {
+                                            "type": "string"
+                                        },
+                                    }
+                                },
+                                "text": {
+                                    "type": "string"
+                                },
+                                "created_at": {
+                                    "format": "EEE MMM dd HH:mm:ss Z yyyy",
+                                    "type": "date"
+                                },
+                                "timestamp_ms": {
+                                    "type": "date"
+                                },
+                                "user": {
+                                    "properties": {
+                                        "created_at": {
+                                            "format": "EEE MMM dd HH:mm:ss Z yyyy",
+                                            "type": "date"
+                                        },
+                                        "description": {
+                                            "type": "string"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+# Twitter
+AUTH = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+AUTH.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
+API = tweepy.API(AUTH)
+
+# Cassandra
+CLUSTER = Cluster(['192.168.2.33'], port=9042)
+SESSION = CLUSTER.connect()
+
+# Elasticsearch
+ES_HOST = {"host": "192.168.2.33", "port": 9200}
+ES = Elasticsearch([ES_HOST])
+
+
+# Function to initialise cassandra manage the table and keyspace creation
+# It will also set the KEYSPACE
+def cassandra_initialisation(session, strategy, replication_factor, keyspace, table_name):
+    request = "CREATE KEYSPACE IF NOT EXISTS {0}" \
+              " WITH replication = {{'class': '{1}', 'replication_factor': '{2}' }}"
+    request = request.format(keyspace, strategy, replication_factor)
+    session.execute(request)
+    session.set_keyspace(keyspace)
+    table = """CREATE TABLE IF NOT EXISTS {} (
+                node_id bigint,
+                tweet_id bigint,
+                json_tweet text,
+                PRIMARY KEY ((node_id), tweet_id)
+            );""".format(table_name)
+    session.execute(table)
+
+
+# Function that save the tweets in Cassandra and Elasticsearch
+def save_tweets(node_id, tweets, session, es):
+    save_tweets_in_cassandra(node_id, tweets, session)
+    save_tweets_in_elasticsearch(tweets, es)
+
+
+# Function to save the "protected user" that doesn't want to make their tweets public
+def save_protected(node_id, session):
+    prepared_statement = session.prepare("INSERT INTO tweets (node_id, tweet_id, json_tweet) VALUES ({}, ?, ?)"
+                                         .format(node_id))
+    session.execute(prepared_statement, (0, "protected"))
+
+
+# Function that save the tweet into Cassandra
+def save_tweets_in_cassandra(node_id, tweets, session):
+    tweets_id = [tweet['id'] for tweet in tweets]
+    tweet_string = [str(tweet) for tweet in tweets]
+    prepared_statement = session.prepare("INSERT INTO tweets (node_id, tweet_id, json_tweet) VALUES ({}, ?, ?)"
+                                         .format(node_id))
+    execute_concurrent_with_args(session, prepared_statement, zip(tweets_id, tweet_string))
+    print("Node: {0} containing {1} tweets save into the database".format(node_id, len(tweets)))
+
+
+# Function that save the tweet into elasticsearch
+def save_tweets_in_elasticsearch(tweets, es):
+    for tweet in tweets:
+        tweet['_id'] = tweet['id']
+        tweet['_op_type'] = "index"
+        tweet['_index'] = "tweets"
+        tweet["_type"] = "tweet"
+    helpers.bulk(actions=tweets, client=es, refresh=True)
+
+
+# Access the tweet of a user via Tweepy and Twitter API
+def get_tweet(node_id):
+    tweets = []
+    for tweet in tweepy.Cursor(API.user_timeline, id=node_id, count=200, include_rts=1,
+                               wait_on_rate_limit=True, wait_on_rate_limit_notify=True).items():
+        tweets.append(tweet)
+    return tweets
+
+
+# Function that process an ID and save the tweets corresponding to this user
+def collect_and_save_tweet_from_user(user_id):
+    query = "SELECT COUNT(*) FROM tweets WHERE node_id={0}".format(user_id)
+    response = SESSION.execute(query)
+    if response[0].count == 0:
+        try:
+            print("Getting node: {}".format(user_id))
+            statuses = get_tweet(user_id)
+            tweets = [status._json for status in statuses]
+            save_tweets(user_id, tweets, SESSION, ES)
+        except Exception:
+            print("Protected user")
+            save_protected(user_id, SESSION)
+            pass
+    else:
+        print("Node: {0}, already fetch".format(user_id))
+
+
+# Initialise Cassandra and elasticsearch
+if not ES.indices.exists("tweets"):
+    ES.indices.create("tweets", body=TWEETS_MAPPING)
+
+cassandra_initialisation(SESSION, "SimpleStrategy", 2, "twitter", "tweets")
+
+
+
